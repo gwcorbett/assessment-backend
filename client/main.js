31845543712685f08fa4e7b8fae7f494cb8e48ca
Greg <gwcorbett@gmail.com>
2022-08-19 10:33:38 -0600

    Startish

---
+++ client/main.js
@@ -0,0 +1,21 @@
+const complimentBtn = document.getElementById("complimentButton")
+const fortuneBtn = document.getElementById("fortuneButton")
+
+const getFortune = () => {
+    axios.get("http://localhost:4000/api/fortune/")
+        .then(res => {
+            const fData = res.fdata;
+            alert(fData);
+        });
+};
+
+const getCompliment = () => {
+    axios.get("http://localhost:4000/api/compliment/")
+        .then(res => {
+            const data = res.data;
+            alert(data);
+    });
+};
+
+complimentBtn.addEventListener('click', getCompliment)
+fortuneBtn.addEventListener('click', getFortune)
